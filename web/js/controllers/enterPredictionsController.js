b44c179628a925a13546dbea65626cf8c962e4d5
Sam Hampton <sam.hampton@advancedcomputersoftware.com>
2016-04-23 14:24:58 +0100

    Add Angular links to head.

--- web/js/controllers/enterPredictionsController.js
+++ web/js/controllers/enterPredictionsController.js
@@ -24,101 +24,121 @@ function enterPredictionsController(
                     $scope.fixture = weekFixtures[$scope.fixtureNo];
                     $scope.noOfGames = weekFixtures.length - 1;
                 });
-        });    
+        });
+
+    $scope.nextFixture = function () {
+        $scope.fixtureNo += 1;
+        $scope.fixture = weekFixtures[$scope.fixtureNo];
+    };
+
+    $scope.prevFixture = function () {
+        $scope.fixtureNo -= 1;
+        $scope.fixture = weekFixtures[$scope.fixtureNo];
+    };
+
+    $scope.submit = function () {
+        var userId = dataService.ref.child("scores/user");
+        var userRef = user.child(dataService.authenticatedUser.uid)
+
+        var matchdayRef = matchday.child(currentMatchday);
+
+        var fixture = matchdayRef.child("fixture");
+
+        var predictions = [];
+
+        $scope.fixtureList.fixtures.forEach(function (fixture) {
+
+            predictions.push({
+                homeTeam: fixture.homeTeamName,
+                homePrediction: fixture.homePrediction == null ? 0 : fixture.homePrediction,
+                awayTeam: fixture.awayTeamName,
+                awayPrediction: fixture.awayPrediction == null ? 0 : fixture.awayPrediction,
+                date: fixture.date,
+                status: fixture.status
+            });
+        });
+        fixture.set(predictions);
+    };
+
+
+    //       $scope.submit = function () {
+    //         var user = dataService.ref.child("scores/user");
+    //         var userRef = user.child(dataService.authenticatedUser.uid)
+
+    //         var matchday = userRef.child("matchday");
+    //         var matchdayRef = matchday.child(currentMatchday);
+
+    //         var fixture = matchdayRef.child("fixture");
+
+    //         var predictions = [];
+
+    //         $scope.fixtureList.fixtures.forEach(function (fixture) {
+
+    //           predictions.push({
+    //             homeTeam: fixture.homeTeamName,
+    //             homePrediction: fixture.homePrediction == null ? 0 : fixture.homePrediction,
+    //             awayTeam: fixture.awayTeamName,
+    //             awayPrediction: fixture.awayPrediction == null ? 0 : fixture.awayPrediction,
+    //             date: fixture.date,
+    //             status: fixture.status
+    //           });
+    //         });
+    //         fixture.set(predictions);
+    //       }
+    //     });
+
+    // getCurrentMatchday()
+    //     .then(function(currentMatchday) {
+    //         $http({
+    //             //headers: { 'X-Auth-Token': 'b435bb252dad4a63ab0ab09b10314773' },
+    //             method: 'GET',
+    //             url: '/viewFixtures'
+    //         }).then(function successCallback(response) {
+    //             $scope.fixtureList = response.data;
+    //         }, function errorCallback(response) {
+    //         });
+    //     });
+
+    // function getCurrentMatchday() {
+    //     return 29;
+    // };
 
 
 
-            $scope.nextFixture = function () {
-                $scope.fixtureNo += 1;
-                $scope.fixture = weekFixtures[$scope.fixtureNo];
-            };
+    //       $scope.submit = function () {
+    //         var user = dataService.ref.child("scores/user");
+    //         var userRef = user.child(dataService.authenticatedUser.uid)
 
-            $scope.prevFixture = function () {
-                $scope.fixtureNo -= 1;
-                $scope.fixture = weekFixtures[$scope.fixtureNo];
-            };
+    //         var matchday = userRef.child("matchday");
+    //         var matchdayRef = matchday.child(currentMatchday);
 
+    //         var fixture = matchdayRef.child("fixture");
 
+    //         var predictions = [];
 
+    //         $scope.fixtureList.fixtures.forEach(function (fixture) {
 
-            //       $scope.submit = function () {
-            //         var user = dataService.ref.child("scores/user");
-            //         var userRef = user.child(dataService.authenticatedUser.uid)
+    //           predictions.push({
+    //             homeTeam: fixture.homeTeamName,
+    //             homePrediction: fixture.homePrediction == null ? 0 : fixture.homePrediction,
+    //             awayTeam: fixture.awayTeamName,
+    //             awayPrediction: fixture.awayPrediction == null ? 0 : fixture.awayPrediction,
+    //             date: fixture.date,
+    //             status: fixture.status
+    //           });
+    //         });
+    //         fixture.set(predictions);
+    //       }
+    //     });
 
-            //         var matchday = userRef.child("matchday");
-            //         var matchdayRef = matchday.child(currentMatchday);
-
-            //         var fixture = matchdayRef.child("fixture");
-
-            //         var predictions = [];
-
-            //         $scope.fixtureList.fixtures.forEach(function (fixture) {
-
-            //           predictions.push({
-            //             homeTeam: fixture.homeTeamName,
-            //             homePrediction: fixture.homePrediction == null ? 0 : fixture.homePrediction,
-            //             awayTeam: fixture.awayTeamName,
-            //             awayPrediction: fixture.awayPrediction == null ? 0 : fixture.awayPrediction,
-            //             date: fixture.date,
-            //             status: fixture.status
-            //           });
-            //         });
-            //         fixture.set(predictions);
-            //       }
-            //     });
-
-            // getCurrentMatchday()
-            //     .then(function(currentMatchday) {
-            //         $http({
-            //             //headers: { 'X-Auth-Token': 'b435bb252dad4a63ab0ab09b10314773' },
-            //             method: 'GET',
-            //             url: '/viewFixtures'
-            //         }).then(function successCallback(response) {
-            //             $scope.fixtureList = response.data;
-            //         }, function errorCallback(response) {
-            //         });
-            //     });
-
-            // function getCurrentMatchday() {
-            //     return 29;
-            // };
-
-
-
-            //       $scope.submit = function () {
-            //         var user = dataService.ref.child("scores/user");
-            //         var userRef = user.child(dataService.authenticatedUser.uid)
-
-            //         var matchday = userRef.child("matchday");
-            //         var matchdayRef = matchday.child(currentMatchday);
-
-            //         var fixture = matchdayRef.child("fixture");
-
-            //         var predictions = [];
-
-            //         $scope.fixtureList.fixtures.forEach(function (fixture) {
-
-            //           predictions.push({
-            //             homeTeam: fixture.homeTeamName,
-            //             homePrediction: fixture.homePrediction == null ? 0 : fixture.homePrediction,
-            //             awayTeam: fixture.awayTeamName,
-            //             awayPrediction: fixture.awayPrediction == null ? 0 : fixture.awayPrediction,
-            //             date: fixture.date,
-            //             status: fixture.status
-            //           });
-            //         });
-            //         fixture.set(predictions);
-            //       }
-            //     });
-
-            //   $scope.matchdayChanged = function () {
-            //     $http({
-            //       headers: { 'X-Auth-Token': 'b435bb252dad4a63ab0ab09b10314773' },
-            //       method: 'GET',
-            //       url: 'http://api.football-data.org/alpha/soccerseasons/398/fixtures/?matchday='.concat($scope.selectedMatchday)
-            //     }).then(function successCallback(response) {
-            //       $scope.fixtureList = response.data;
-            //     }, function errorCallback(response) {
-            //     });
-            //   }
-        };
+    //   $scope.matchdayChanged = function () {
+    //     $http({
+    //       headers: { 'X-Auth-Token': 'b435bb252dad4a63ab0ab09b10314773' },
+    //       method: 'GET',
+    //       url: 'http://api.football-data.org/alpha/soccerseasons/398/fixtures/?matchday='.concat($scope.selectedMatchday)
+    //     }).then(function successCallback(response) {
+    //       $scope.fixtureList = response.data;
+    //     }, function errorCallback(response) {
+    //     });
+    //   }
+};
